c553afb68244f85d03f0cc3cc8004794b32a4b60
kinny <kinnyhuang529@gmail.com>
2023-12-18 15:13:09 +0800

    fix: 變數名稱不合理問題

--- example/main.go
+++ example/main.go
@@ -12,19 +12,21 @@ func main() {
 	//洗牌
 	evaluator.ShuffleDeck(deck)
 	fmt.Println("shuffle deck: ", deck)
-	//抽牌
-	hand := evaluator.DrawCards(deck, 5)
+	//手牌
+	hand := evaluator.DrawCards(deck, 2)
 	fmt.Println("hand: ", hand)
-	//開始算牌 5張
-	cardType, cardStrength := evaluator.Evaluator(hand)
-	fmt.Println("5 cards card type: ", cardType)         //牌型
-	fmt.Println("5 cards card strength: ", cardStrength) //牌力 牌力數值越小表示牌型越大
 	//公牌
-	public := evaluator.DrawCards(deck, 2)
+	public := evaluator.DrawCards(deck, 5)
 	fmt.Println("public: ", public)
 	//把手牌+公牌
 	cards := append(hand, public...)
 	fmt.Println("cards: ", cards)
+
+	var cardType, cardStrength int
+	//開始算牌 5張
+	cardType, cardStrength = evaluator.Evaluator(public)
+	fmt.Println("5 cards card type: ", cardType)         //牌型
+	fmt.Println("5 cards card strength: ", cardStrength) //牌力 牌力數值越小表示牌型越大
 	//開始算牌 7張
 	cardType, cardStrength = evaluator.HoldemEvaluator(cards)
 	fmt.Println("7 cards card type: ", cardType)         //牌型
